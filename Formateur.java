42e091cef8f921328ef624accb11ce77a39185d6
YassineMlayah <178038860+YassineMlayah@users.noreply.github.com>
2024-11-21 12:55:02 +0100

    class Formateur init

---
+++ Formateur.java
@@ -0,0 +1,9 @@
+public class Formateur extends Utilisateur {
+        //ATTRIBUTS
+    private Formation[] formations;
+
+        //METHODES
+    void ajouterFormation(Formation formation){
+        
+    }
+}
